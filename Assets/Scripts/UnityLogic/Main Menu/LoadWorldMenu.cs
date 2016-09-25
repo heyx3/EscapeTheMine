7562e3b2b9461fad7da0f40d8a793e2662cd329b
heyx3 <manning.w@husky.neu.edu>
2016-09-25 13:08:22 -0400

    Work on menu/unity logic

---
+++ Assets/Scripts/UnityLogic/Main Menu/LoadWorldMenu.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using UnityEngine;
+
+
+namespace UnityLogic
+{
+	public class LoadWorldMenu : Singleton<LoadWorldMenu>
+	{
+		public const string SaveFolderName = "Saves";
+
+		public string SaveFolderPath { get { return Path.Combine(Application.dataPath, SaveFolderName); } }
+
+
+		public UnityEngine.UI.Dropdown SelectionDropdown;
+
+		[NonSerialized]
+		public string SelectedWorld = null;
+
+		private List<string> availableWorlds;
+
+
+		public void Callback_SelectionChanged(int newIndex)
+		{
+			SelectedWorld = availableWorlds[newIndex];
+		}
+
+		private void OnEnable()
+		{
+			if (!Directory.Exists(SaveFolderPath))
+				Directory.CreateDirectory(SaveFolderPath);
+
+			string[] worldFiles = Directory.GetFiles(SaveFolderPath, "*.world");
+			availableWorlds = worldFiles.Select<string, string>(Path.GetFileNameWithoutExtension).ToList();
+
+			SelectionDropdown.options = availableWorlds.Select(s => new UnityEngine.UI.Dropdown.OptionData(s)).ToList();
+
+			if (availableWorlds.Count > 0)
+				SelectedWorld = availableWorlds[0];
+			else
+				SelectedWorld = null;
+		}
+	}
+}
---
+++ Assets/Scripts/UnityLogic/Main Menu/MainMenu.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using UnityEngine;
+
+
+namespace UnityLogic
+{
+	public class MainMenu : Singleton<MainMenu>
+	{
+		public GameObject Menu_Main, Menu_NewWorld, Menu_LoadWorld;
+
+
+		public void Callback_SetupNewGame()
+		{
+			Activate(Menu_NewWorld);
+		}
+		public void Callback_LoadGame()
+		{
+			Activate(Menu_LoadWorld);
+		}
+		public void Callback_Quit()
+		{
+			Application.Quit();
+		}
+
+		public void Callback_ToMainMenu()
+		{
+			Activate(Menu_Main);
+		}
+
+		public void Callback_StartNewGame()
+		{
+			GameFSM.Instance.GenerateWorld();
+		}
+		public void Callback_StartLoadedGame()
+		{
+			GameFSM.Instance.LoadWorld(Path.Combine(LoadWorldMenu.Instance.SaveFolderPath,
+													LoadWorldMenu.Instance.SelectedWorld));
+		}
+
+		
+		private void Activate(GameObject menu)
+		{
+			Menu_Main.SetActive(Menu_Main == menu);
+			Menu_NewWorld.SetActive(Menu_NewWorld == menu);
+			Menu_LoadWorld.SetActive(Menu_LoadWorld == menu);
+		}
+
+
+		private void Start()
+		{
+			Activate(Menu_Main);
+		}
+	}
+}
