9ef8ca80a5fb85621e80deeccb895c9aec4411d2
heyx3 <manning.w@husky.neu.edu>
2016-11-28 23:57:54 -0500

    Add Paused vignette
    Made placeholder tilesheet less red so the vignette is more visible

---
+++ Assets/Scripts/UI/GamePausedOverlay.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+
+namespace MyUI
+{
+	public class GamePausedOverlay : MonoBehaviour
+	{
+		private UnityLogic.EtMGame Game { get { return UnityLogic.EtMGame.Instance; } }
+
+
+		private void Start()
+		{
+			gameObject.SetActive(Game.IsInGame && Game.Map.IsPaused);
+
+			Game.Map.IsPaused.OnChanged += Callback_PauseToggled;
+			Game.OnStart += Callback_MapStart;
+			Game.OnEnd += Callback_MapEnd;
+		}
+
+		private void Callback_PauseToggled(GameLogic.Map map, bool oldVal, bool newVal)
+		{
+			gameObject.SetActive(newVal);
+		}
+		private void Callback_MapStart()
+		{
+			gameObject.SetActive(Game.Map.IsPaused);
+		}
+		private void Callback_MapEnd()
+		{
+			gameObject.SetActive(false);
+		}
+	}
+}
